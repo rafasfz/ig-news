ffd5c5cda4904957342b0ae3c7dac91f4d7d0681
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-12 16:18:35 -0300

    feat: add scss and document component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+  <Head>
+    <title>ig.news</title>
+  </Head>
+  <h1 className={styles.title}>
+    Hello<span> World</span>
+  </h1>
+  </>)
 }
