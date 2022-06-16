357cdf0fbcf8a3b9e9114e955d8cd55f7b21589e
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-16 15:02:06 -0300

    feat: Add next auth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,26 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession();
 
-  return isUserLoggedIn ? 
+  return status === 'authenticated' ? 
   <button
     className={styles.signInButton}
     type="button"
   >
     <FaGithub color="#04d361" />
-    Rafael Freire
-    <FiX color="#737380" className={styles.closeIcon} />
+    {session.user.name}
+    <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()}/>
   </button>
   : (
     <button
       className={styles.signInButton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
