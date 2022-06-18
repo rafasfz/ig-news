6674e21fddab8b66d13054bf969ea41d6793e0d2
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-18 16:56:54 -0300

    feat: Add stripe checkout when click in subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,19 +10,30 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session, status } = useSession();
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn('github');
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
