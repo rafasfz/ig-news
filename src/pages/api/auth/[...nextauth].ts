bda5060b46854bfedc0b53770433b58622f247b1
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-18 01:03:54 -0300

    feat: register user in fauna db when sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,25 @@ export default NextAuth({
       }
     }),
   ],
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+      } catch {
+        return false;
+      }
+
+      return true;
+    }
+  }
 })
