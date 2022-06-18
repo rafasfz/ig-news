8cf925667cd82603fd602978cb56ea31915fb487
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-18 15:52:53 -0300

    feat: check if user exist before create user in fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,18 +16,31 @@ export default NextAuth({
       }
     }),
   ],
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
           )
         );
       } catch {
